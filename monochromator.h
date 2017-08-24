d344c7fb68ae8d69b2e2784f8f0384dcc021f44d
Andres <andres@localhost.localdomain>
2017-08-24 13:21:00 -0400

    First commit

---
+++ monochromator.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   monochromator.h
+ * Author: andres
+ *
+ * Created on August 24, 2017, 1:16 PM
+ */
+
+#ifndef MONOCHROMATOR_H
+#define MONOCHROMATOR_H
+
+
+
+#endif /* MONOCHROMATOR_H */
+
---
+++ resources.h
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   resources.h
+ * Author: andres
+ *
+ * Created on August 24, 2017, 1:16 PM
+ * This file contains the USB controls parameters for the 
+ * Monochromator iHR320
+ */
+
+#ifndef RESOURCES_H
+#define RESOURCES_H
+
+#define IDM_ABOUTBOX    0x0010
+
+#endif /* RESOURCES_H */
+
